bad395e1dc619cf67acba77b0f2da3a70ec64e7d
Ömer <36486345+Superpace@users.noreply.github.com>
2020-05-31 15:25:00 +0300

    Update wpeditor.py

--- wpeditor.py
+++ wpeditor.py
@@ -6,30 +6,31 @@ kelimeler = list()
 # AynÄ± klasÃ¶rde olan dosyamÄ±zÄ± hem okuma hem de yazma olanaÄŸÄ± sunan
 # W+ modunda aÃ§Ä±yoruz
 veri=whatsappTxt.read()
-satirlar=veri.split("\n") #veri deÄŸiÅŸkenindeki \n lere gÃ¶re parÃ§alayÄ±p
-                         #veriler deÄŸiÅŸkenine atar ve veriler deÄŸiÅŸkeni
-                         #artÄ±k dizi olur
+satirlar=veri.split("\n") #veri deÄŸiÅŸkenini \n lere gÃ¶re parÃ§alayÄ±p
+                         #veriler deÄŸiÅŸkenine atarÄ±z ve veriler deÄŸiÅŸkeni
+                         #artÄ±k bir dizi olur
                          
-for i in range(len(satirlar)): #Satırların içinde dolanıyoruz
+for i in range(len(satirlar)): #SatÄ±rlarÄ±n iÃ§inde dolanÄ±yoruz
    
-    if satirlar[2] == ".":             #Burada iki haneli ve tek haneli tarihleri ayırdık
-        satirlar[i] = satirlar[i][19:] #Ve bunlara göre satırlarımızı isime kadar kırptık
+    if satirlar[2] == ".":             #Burada iki haneli ve tek haneli tarihleri ayÄ±rdÄ±k
+        satirlar[i] = satirlar[i][19:] #Ve bunlara gÃ¶re satÄ±rlarÄ±mÄ±zÄ± isime kadar kÄ±rptÄ±k
                                        #14.02.2020 11:35 - 
     else :
         satirlar[i] = satirlar[i][18:] #1.04.2020 13:45 - 
     
     if satirlar[i][0:len(ISIM)] == ISIM and satirlar[i][0:len(ISIM)+8] != ISIM+": <Medya" :
-        #Önce aradığımız isimdeki satırları bulduk VE 
-        #Sonra Medya dahil edilmedi yazılarını seçim dışı bıraktık
-        #( Örn: 1.04.2020 13:32 - Batuhan: <Medya )
-        cumleler.append(satirlar[i][len(ISIM)+2:]) #İsim kısmını ve : lı kısmı kırparak 
-                                                   #asıl cümleyi listeye ekledik
+        #Ã–nce aradÄ±ÄŸÄ±mÄ±z isimdeki satÄ±rlarÄ± bulduk VE 
+        #Sonra Medya dahil edilmedi yazÄ±larÄ±nÄ± seÃ§im dÄ±ÅŸÄ± bÄ±raktÄ±k
+        #( Ã–rn: 1.04.2020 13:32 - Batuhan: <Medya )
+        cumleler.append(satirlar[i][len(ISIM)+2:]) #Ä°sim kÄ±smÄ±nÄ± ve : lÄ± kÄ±smÄ± kÄ±rparak 
+                                                   #asÄ±l cÃ¼mleyi listeye ekledik
             
-for x in range(len(cumleler)): # Cümlelerin içinde geziyoruz
-    for y in range(len(cumleler[x].split())): #2. 
+for x in range(len(cumleler)): # CÃ¼mlelerin iÃ§inde geziyoruz
+    for y in range(len(cumleler[x].split())): #2. for'u aÃ§tÄ±k
         if cumleler[x][0:4] != "http" and len(cumleler[x].split()[y])<17 and not cumleler[x].split(" ")[y][0].isdigit():
-            kelimeler.append(cumleler[x].split(" ")[y].lower())
+                 # EÄŸer bir link      ve   harf uzunluÄŸu 17 Ã¼zeri         ve     kelime diye aldÄ±klarÄ±mÄ±z bir sayÄ± deÄŸilse
+            kelimeler.append(cumleler[x].split(" ")[y].lower()) # Kelimeler dizimize ekledik
 
 
 whatsappTxt.close()
-#Son olarak dosyamızı kapatıyoruz
+#Son olarak dosyamÄ±zÄ± kapatÄ±yoruz
