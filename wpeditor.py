dfc8893c0e9eb1b8ad1009afccff2e07855a9af7
Superpace <omerozturkunmaili@gmail.com>
2020-05-31 15:10:03 +0300

    İlk Commit

---
+++ wpeditor.py
@@ -0,0 +1,35 @@
+
+ISIM = "Batuhan" #Sohbet ismini seÃ§tik
+whatsappTxt = open("deneme1.txt","r+",encoding="utf8") 
+cumleler = list()
+kelimeler = list()
+# AynÄ± klasÃ¶rde olan dosyamÄ±zÄ± hem okuma hem de yazma olanaÄŸÄ± sunan
+# W+ modunda aÃ§Ä±yoruz
+veri=whatsappTxt.read()
+satirlar=veri.split("\n") #veri deÄŸiÅŸkenindeki \n lere gÃ¶re parÃ§alayÄ±p
+                         #veriler deÄŸiÅŸkenine atar ve veriler deÄŸiÅŸkeni
+                         #artÄ±k dizi olur
+                         
+for i in range(len(satirlar)): #Satırların içinde dolanıyoruz
+   
+    if satirlar[2] == ".":             #Burada iki haneli ve tek haneli tarihleri ayırdık
+        satirlar[i] = satirlar[i][19:] #Ve bunlara göre satırlarımızı isime kadar kırptık
+                                       #14.02.2020 11:35 - 
+    else :
+        satirlar[i] = satirlar[i][18:] #1.04.2020 13:45 - 
+    
+    if satirlar[i][0:len(ISIM)] == ISIM and satirlar[i][0:len(ISIM)+8] != ISIM+": <Medya" :
+        #Önce aradığımız isimdeki satırları bulduk VE 
+        #Sonra Medya dahil edilmedi yazılarını seçim dışı bıraktık
+        #( Örn: 1.04.2020 13:32 - Batuhan: <Medya )
+        cumleler.append(satirlar[i][len(ISIM)+2:]) #İsim kısmını ve : lı kısmı kırparak 
+                                                   #asıl cümleyi listeye ekledik
+            
+for x in range(len(cumleler)): # Cümlelerin içinde geziyoruz
+    for y in range(len(cumleler[x].split())): #2. 
+        if cumleler[x][0:4] != "http" and len(cumleler[x].split()[y])<17 and not cumleler[x].split(" ")[y][0].isdigit():
+            kelimeler.append(cumleler[x].split(" ")[y].lower())
+
+
+whatsappTxt.close()
+#Son olarak dosyamızı kapatıyoruz
